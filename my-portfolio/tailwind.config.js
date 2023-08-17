fb8f75ed8db802465356887904a59fa4030b7c1e
Phương <20521779@gm.uit.edu.vn>
2023-08-17 13:02:39 +0700

    commit

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    fontFamily: {
+      vt323: ["VT323"],
+      heading: ["Lilita One"],
+      desc: ["Ubuntu Mono"],
+      sans: ["Open Sans"],
+    },
   },
   plugins: [],
 };
